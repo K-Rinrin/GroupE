10e319f57511128566c4c3516ba2ef81fcda47a1
Karin <0828karn@gmeil.com>
2025-11-21 15:40:46 +0900

    asdfg

--- oshiro_navi/event_info_management/views.py
+++ oshiro_navi/event_info_management/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+# class OperatorTopView(TemplateView):
+#     template_name = "operator_top.html"
+
+class EventInfoManagementView(TemplateView):
+    template_name = "event_info_management.html"
