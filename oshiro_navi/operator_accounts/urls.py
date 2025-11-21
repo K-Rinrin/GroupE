41c1cb025295fc4654bf6eb002e046df88ab22bf
Karin <0828karn@gmeil.com>
2025-11-21 14:30:16 +0900

    bu

--- oshiro_navi/operator_accounts/urls.py
+++ oshiro_navi/operator_accounts/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('top/', TopView.as_view(), name='topview'),
+    path('top/', OperatorTopView.as_view(), name='topview'),
 ]
