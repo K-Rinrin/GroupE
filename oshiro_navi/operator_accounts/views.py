7938ff03689fc63aa6365568feea14bf095a694e
moto57-f <mtms.0507@gmail.com>
2025-11-21 16:42:14 +0900

    komitto

--- oshiro_navi/operator_accounts/views.py
+++ oshiro_navi/operator_accounts/views.py
@@ -8,3 +8,21 @@ class OperatorTopView(TemplateView):
 
 class AdminAccountListView(TemplateView):
     template_name = "admin_account_list.html"
+
+class AdminAccountCreateView(TemplateView):
+    template_name = "admin_account_create.html"
+
+class AdminAccountCreateSuccessView(TemplateView):
+    template_name = "admin_account_create_success.html"
+
+class AdminAccountDeleteView(TemplateView):
+    template_name = "admin_account_delete.html"
+
+class AdminAccountDeleteSuccessView(TemplateView):
+    template_name = "admin_account_delete_success.html"
+
+class OperatorLoginView(TemplateView):
+    template_name = "operator_login.html"
+
+class OperatorLogoutView(TemplateView):
+    template_name = "operator_logout.html"
