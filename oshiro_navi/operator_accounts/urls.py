302343cbcf2ecd4bdfb40d7fa4e29299d0778394
moto57-f <mtms.0507@gmail.com>
2025-11-21 16:01:05 +0900

    naosimasita

--- oshiro_navi/operator_accounts/urls.py
+++ oshiro_navi/operator_accounts/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('top/', OperatorTopView.as_view(), name='topview'),
+    path('operatortop/', OperatorTopView.as_view(), name='operatortop'),
+    path('operatortop/account_list', AdminAccountListView.as_view(), name='account_list'),
+
 ]
--- oshiro_navi/operator_accounts/views.py
+++ oshiro_navi/operator_accounts/views.py
@@ -5,3 +5,6 @@ from django.views.generic.base import TemplateView
 # Create your views here.
 class OperatorTopView(TemplateView):
     template_name = "operator_top.html"
+
+class AdminAccountListView(TemplateView):
+    template_name = "admin_account_list.html"
