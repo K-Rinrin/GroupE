fdf33839dae41f73b8834b924d127b4a7cdd4227
KannaSaito <amahara171@gmail.com>
2025-11-25 13:51:52 +0900

    a

--- oshiro_navi/admin_accounts/urls.py
+++ oshiro_navi/admin_accounts/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import *
 
+app_name = "admin_accounts"
+
 urlpatterns = [
-    path('top/', AdminTopView.as_view(), name='admintop'),
+    path('top/', AdminTopView.as_view(), name='top'),
     
 
 ]
