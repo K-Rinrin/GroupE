a601a17b411091d106b1f841048f4297a5c5087a
KannaSaito <amahara171@gmail.com>
2025-11-25 09:52:38 +0900

    a

--- oshiro_navi/admin_area_map/urls.py
+++ oshiro_navi/admin_area_map/urls.py
@@ -1,16 +1,8 @@
 from django.urls import path
-<<<<<<< HEAD
-=======
-
->>>>>>> 6fa841c51720820f166cac0d766a9443413780d4
 from .views import *
 
 app_name = 'admin_area_map'
 
 urlpatterns = [
-<<<<<<< HEAD
     path('area_map/', AreaMapInfoListView.as_view(), name='area_map'),
 ]
-=======
-]
->>>>>>> 6fa841c51720820f166cac0d766a9443413780d4
