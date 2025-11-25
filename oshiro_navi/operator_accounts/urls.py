bbe0068fd1bb78b59c3ff132ccb9f9ae628ff8aa
moto57-f <mtms.0507@gmail.com>
2025-11-25 10:18:44 +0900

    ope

--- oshiro_navi/operator_accounts/urls.py
+++ oshiro_navi/operator_accounts/urls.py
@@ -2,10 +2,16 @@ from django.urls import path
 
 from .views import *
 
-urlpatterns = [
-    path('top/', OperatorTopView.as_view(), name='top'),
-    path('top/list', AdminAccountListView.as_view(), name='account_list'),
-    path('top/list/delete', AdminAccountDeleteView.as_view(), name='account_delete'),
+app_name = "operator_accounts"
 
+urlpatterns = [
+    path('',OperatorLoginView.as_view(), name='operator_login'),
+    path('logout/',OperatorLogoutView.as_view(), name='operator_logout'),
+    path('top/', OperatorTopView.as_view(), name='top'),
+    path('top/list/', AdminAccountListView.as_view(), name='account_list'),
+    path('top/list/create/',AdminAccountCreateView.as_view(), name='account_create'),
+    path('top/list/create/success/',AdminAccountCreateSuccessView.as_view(), name='account_create_success'),
+    path('top/list/delete/', AdminAccountDeleteView.as_view(), name='account_delete'),
+    path('top/list/delete/success/', AdminAccountDeleteSuccessView.as_view(), name='account_delete_success'),
 
 ]
