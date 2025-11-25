684530f5b17548b0f21a4e514a5d3a3a980e6226
KannaSaito <amahara171@gmail.com>
2025-11-25 12:13:28 +0900

    a

--- oshiro_navi/admin_area_map/urls.py
+++ oshiro_navi/admin_area_map/urls.py
@@ -4,6 +4,35 @@ from .views import *
 app_name = 'admin_area_map'
 
 urlpatterns = [
+
+    # 周辺MAP一覧ページ
     path('area_map/', AreaMapInfoListView.as_view(), name='area_map'),
+
+
+    # 周辺MAP登録ページ
+    path('area_map_info_registar/', 
+         AreaMapInfoRegistarView.as_view(), name='area_map_info_registar'),
+
+    # 周辺MAP登録完了ページ
+    path('area_map_info_registar_success/', 
+         AreaMapInfoRegistarSuccessView.as_view(), name='area_map_info_registar_success'),
+
+
+    # 周辺MAP更新ページ
+    path('area_map_info_update/', 
+         AreaMapInfoUpdateView.as_view(), name='area_map_info_update'),
+
+    # 周辺MAP更新完了ページ
+    path('area_map_info_update_success/', 
+         AreaMapInfoUpdateSuccessView.as_view(), name='area_map_info_update_success'),
+
+
+    # 周辺MAP削除ページ
+    path('area_map_info_delete/', 
+         AreaMapInfoDeleteView.as_view(), name='area_map_info_delete'),
+
+    # 周辺MAP削除完了ページ
+    path('area_map_info_delete_success/', 
+         AreaMapInfoDeleteSuccessView.as_view(), name='area_map_info_delete_success'),
     
 ]
--- oshiro_navi/admin_area_map/views.py
+++ oshiro_navi/admin_area_map/views.py
@@ -5,3 +5,31 @@ from django.views.generic.base import TemplateView
 # Create your views here.
 class AreaMapInfoListView(TemplateView):
     template_name = "area_map_info_list.html"
+
+# 登録画面
+class AreaMapInfoRegistarView(TemplateView):
+    template_name = "area_map_info_registar.html"
+
+# 登録完了画面
+class AreaMapInfoRegistarSuccessView(TemplateView):
+    template_name = "area_map_info_registar_success.html"
+
+
+
+# 更新画面
+class AreaMapInfoUpdateView(TemplateView):
+    template_name = "area_map_info_update.html"
+
+# 更新完了画面
+class AreaMapInfoUpdateSuccessView(TemplateView):
+    template_name = "area_map_info_update_success.html"
+
+
+
+# 削除画面
+class AreaMapInfoDeleteView(TemplateView):
+    template_name = "area_map_info_delete.html"
+
+# 削除完了画面
+class AreaMapInfoDeleteSuccessView(TemplateView):
+    template_name = "area_map_info_delete_success.html"
