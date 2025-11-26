c206b2917a31d0e7ae9d4e11d0f7ef0b81d8b123
reto1105 <reto82222@gmail.com>
2025-11-26 10:00:05 +0900

    user_model_course

--- oshiro_navi/user_model_course/views.py
+++ oshiro_navi/user_model_course/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class UserModelCourseListView(TemplateView):
+    template_name = "model_course_list.html"
+
+class UserModelCourseDetailView(TemplateView):
+    template_name = "model_course_detail.html"
+
+class UserModelCourseAfterSearchView(TemplateView):
+    template_name = "model_course_after_search.html"
+
+class UsermodelCourseChooseView(TemplateView):
+    template_name = "model_course_choose.html"
+
+
