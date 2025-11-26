f86445a6f02785b71055bd24208ae24a34321024
KannaSaito <amahara171@gmail.com>
2025-11-26 11:39:10 +0900

    user

--- oshiro_navi/static/JS/user.js
+++ oshiro_navi/static/JS/user.js
@@ -1,26 +1,4 @@
 
-document.getElementById("imageInput").addEventListener("change", function(e) {
-    const file = e.target.files[0];
-    const preview = document.getElementById("preview");
-
-    // ファイルが選択されていない場合
-    if (!file) {
-        preview.innerHTML = "画像プレビューエリア";
-        preview.style.backgroundImage = "";
-        return;
-    }
-
-    // 画像URLを生成してプレビュー
-    const imgURL = URL.createObjectURL(file);
-
-    preview.style.backgroundImage = `url(${imgURL})`;
-    preview.style.backgroundSize = "contain";
-    preview.style.backgroundRepeat = "no-repeat";
-    preview.style.backgroundPosition = "center";
-
-    preview.innerHTML = ""; // テキストを消す
-});
-
 document.addEventListener("DOMContentLoaded", () => {
 
     const dropdown = document.querySelector(".dropdown");
