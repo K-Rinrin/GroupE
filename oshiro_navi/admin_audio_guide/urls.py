8e798b98239aed0d660d3826e668b700cdedcc4c
Karin <0828karn@gmeil.com>
2026-01-15 11:54:34 +0900

    admjkikn

--- oshiro_navi/admin_audio_guide/urls.py
+++ oshiro_navi/admin_audio_guide/urls.py
@@ -5,38 +5,38 @@ app_name = 'admin_audio_guide'
 
 urlpatterns = [
 
-    # 音声ガイド一覧ページ
-    path('audio_guide/', AudioGuideListtView.as_view(), name='audio_guide'),
+# 音声ガイド一覧ページ
+path('audio_guide/', AudioGuideListtView.as_view(), name='audio_guide'),
 
 
-    # 音声ガイドP登録ページ
-    path('audio_guide_registar/', 
-         AudioGuideRegistarView.as_view(), name='audio_guide_registar'),
+# 音声ガイドP登録ページ
+path('audio_guide_registar/', 
+     AudioGuideRegistarView.as_view(), name='audio_guide_registar'),
 
-    # 周辺MAP登録完了ページ
-    path('audio_guide_registar_success/', 
-         AudioGuideRegistarSuccessView.as_view(), name='audio_guide_registar_success'),
-
-
-    # 周辺MAP更新ページ
-    path('audio_guide_update/', 
-         AudioGuideUpdateView.as_view(), name='audio_guide_update'),
-
-    # 周辺MAP更新完了ページ
-    path('audio_guide_update_success/', 
-         AudioGuideUpdateSuccessView.as_view(), name='audio_guide_update_success'),
+# 周辺MAP登録完了ページ
+path('audio_guide_registar_success/', 
+     AudioGuideRegistarSuccessView.as_view(), name='audio_guide_registar_success'),
 
 
-    # 周辺MAP削除ページ
-    path('audio_guide_delete/', 
-         AudioGuideDeleteView.as_view(), name='audio_guide_delete'),
+# 周辺MAP更新ページ
+path('audio_guide_update/', 
+     AudioGuideUpdateView.as_view(), name='audio_guide_update'),
 
-    # 周辺MAP削除完了ページ
-    path('audio_guide_delete_success/', 
-         AudioGuideUpdateSuccessView.as_view(), name='audio_guide_delete_success'),
+# 周辺MAP更新完了ページ
+path('audio_guide_update_success/', 
+     AudioGuideUpdateSuccessView.as_view(), name='audio_guide_update_success'),
+
+
+# 周辺MAP削除ページ
+path('audio_guide_delete/', 
+     AudioGuideDeleteView.as_view(), name='audio_guide_delete'),
+
+# 周辺MAP削除完了ページ
+path('audio_guide_delete_success/', 
+     AudioGuideUpdateSuccessView.as_view(), name='audio_guide_delete_success'),
 
      # QRコード生成ページ
-    path('qr_generetor/',
-         QRcodeGeneretorView.as_view(), name='qr_generetor'),
-    
+path('qr_generetor/',
+     QRcodeGeneretorView.as_view(), name='qr_generetor'),
+
 ]
