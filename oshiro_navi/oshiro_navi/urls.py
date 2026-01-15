d82dbd62dbe16d46f94802b969c6e060f349da3e
reto1105 <reto82222@gmail.com>
2026-01-15 11:47:24 +0900

    userprofile

--- oshiro_navi/oshiro_navi/urls.py
+++ oshiro_navi/oshiro_navi/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings 
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('admin_site/', admin.site.urls),
@@ -65,3 +67,5 @@ urlpatterns = [
     path('event_management/', include('event_info_management.urls')),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
