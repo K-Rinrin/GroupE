c7fb373c7aa6cc42dfd7b6ee69cd823065d42b5c
moto57-f <mtms.0507@gmail.com>
2026-01-16 14:48:34 +0900

    f

--- oshiro_navi/admin_oshiro_stamp/admin.py
+++ oshiro_navi/admin_oshiro_stamp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import OshiroStamp
+from .models import OshiroStamp,OshiroStampInfo
 
 admin.site.register(OshiroStamp)
+
+admin.site.register(OshiroStampInfo)
