abb781fe69dabd6fa506989cfda41dd1997e66ca
moto57-f <mtms.0507@gmail.com>
2026-01-20 10:44:34 +0900

    event_info

--- oshiro_navi/event_info_management/urls.py
+++ oshiro_navi/event_info_management/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path('admin/list/', AdminEventInfoListView.as_view(), name='admin_event_info_list'),
     path('admin/register/',AdminEventInfoRegisterView.as_view(), name='admin_event_info_register'),
     path('admin/register_success/',AdminEventInfoRegisterSuccessView.as_view(), name='admin_event_info_register_success'),
-    path('admin/update/',AdminEventInfoUpdateView.as_view(), name='admin_event_info_update'),
+    path('admin/update/<int:pk>/', AdminEventInfoUpdateView.as_view(), name='admin_event_info_update'),
     path('admin/update_success/',AdminEventInfoUpdateSuccessView.as_view(), name='admin_event_info_update_success'),
-    path('admin/delete/',AdminEventInfoDeleteView.as_view(), name='admin_event_info_delete'),
-    path('admin/delete_check/',AdminEventInfoDeleteCheckView.as_view(), name='admin_event_info_delete_check'),
+    path('admin/delete/<int:pk>/', AdminEventInfoDeleteView.as_view(), name='admin_event_info_delete'),
     path('admin/delete_success/',AdminEventInfoDeleteSuccessView.as_view(), name='admin_event_info_delete_success'),
+    path('Admin/detail/<int:pk>/',AdminEventInfoDetailView.as_view(), name='admin_event_info_detail'),
 
     path('operator/list/', OperatorEventInfoListView.as_view(), name='operator_event_info_list'),
     path('operator/register/',OperatorEventInfoRegisterView.as_view(), name='operator_event_info_register'),
@@ -20,6 +20,6 @@ urlpatterns = [
     path('operator/update/<int:pk>/',OperatorEventInfoUpdateView.as_view(), name='operator_event_info_update'),
     path('operator/update_success/',OperatorEventInfoUpdateSuccessView.as_view(), name='operator_event_info_update_success'),
     path('operator/delete/<int:pk>/',OperatorEventInfoDeleteView.as_view(), name='operator_event_info_delete'),
-    path('operator/delete_check/',OperatorEventInfoDeleteCheckView.as_view(), name='operator_event_info_delete_check'),
     path('operator/delete_success/',OperatorEventInfoDeleteSuccessView.as_view(), name='operator_event_info_delete_success'),
+    path('operator/detail/<int:pk>/',OperatorEventInfoDetailView.as_view(), name='operator_event_info_detail'),
 ]
